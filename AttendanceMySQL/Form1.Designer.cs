55a8cc2f3845ce0d749ea2beb25590451e856833
Marc Edison <suanieeee@yahoo.com>
2023-04-30 19:08:06 +0800

    Add project files.

---
+++ AttendanceMySQL/Form1.Designer.cs
@@ -0,0 +1,217 @@
+﻿
+namespace AttendanceMySQL
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtID = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtCourse = new System.Windows.Forms.TextBox();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.toolStripStatusLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.tsDT = new System.Windows.Forms.ToolStripStatusLabel();
+            this.btnReset = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            this.statusStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // txtID
+            // 
+            this.txtID.Font = new System.Drawing.Font("Calibri", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtID.Location = new System.Drawing.Point(46, 106);
+            this.txtID.Name = "txtID";
+            this.txtID.Size = new System.Drawing.Size(278, 37);
+            this.txtID.TabIndex = 0;
+            this.txtID.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.txtID.KeyDown += new System.Windows.Forms.KeyEventHandler(this.IDEnter);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Archivo Narrow SemiBold", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Black;
+            this.label1.Location = new System.Drawing.Point(139, 80);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(98, 26);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "ID NUMBER";
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.ScrollBar;
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.panel1.Controls.Add(this.label4);
+            this.panel1.Controls.Add(this.txtCourse);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.txtName);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.txtID);
+            this.panel1.Location = new System.Drawing.Point(12, 12);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(377, 371);
+            this.panel1.TabIndex = 2;
+            // 
+            // txtName
+            // 
+            this.txtName.Enabled = false;
+            this.txtName.Font = new System.Drawing.Font("Calibri", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtName.Location = new System.Drawing.Point(46, 199);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(278, 37);
+            this.txtName.TabIndex = 1;
+            this.txtName.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.txtName.KeyDown += new System.Windows.Forms.KeyEventHandler(this.NameEnter);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Calibri", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.Black;
+            this.label3.Location = new System.Drawing.Point(40, 18);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(290, 36);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "ATTENDANCE CHECKER";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Archivo Narrow SemiBold", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.Black;
+            this.label2.Location = new System.Drawing.Point(159, 173);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(57, 26);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "NAME";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Archivo Narrow SemiBold", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.Color.Black;
+            this.label4.Location = new System.Drawing.Point(153, 270);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(77, 26);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "COURSE";
+            // 
+            // txtCourse
+            // 
+            this.txtCourse.Enabled = false;
+            this.txtCourse.Font = new System.Drawing.Font("Calibri", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtCourse.Location = new System.Drawing.Point(46, 296);
+            this.txtCourse.Name = "txtCourse";
+            this.txtCourse.Size = new System.Drawing.Size(278, 37);
+            this.txtCourse.TabIndex = 2;
+            this.txtCourse.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.txtCourse.KeyDown += new System.Windows.Forms.KeyEventHandler(this.CourseEnter);
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.BackColor = System.Drawing.Color.White;
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripStatusLabel1,
+            this.tsDT});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 430);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(401, 22);
+            this.statusStrip1.TabIndex = 3;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // toolStripStatusLabel1
+            // 
+            this.toolStripStatusLabel1.Font = new System.Drawing.Font("Calibri", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.toolStripStatusLabel1.Name = "toolStripStatusLabel1";
+            this.toolStripStatusLabel1.Size = new System.Drawing.Size(193, 17);
+            this.toolStripStatusLabel1.Spring = true;
+            this.toolStripStatusLabel1.Text = "MADE BY: @SDEVMARC";
+            // 
+            // tsDT
+            // 
+            this.tsDT.Font = new System.Drawing.Font("Calibri", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tsDT.Name = "tsDT";
+            this.tsDT.Size = new System.Drawing.Size(193, 17);
+            this.tsDT.Spring = true;
+            this.tsDT.Text = "DATE AND TIME";
+            // 
+            // btnReset
+            // 
+            this.btnReset.Font = new System.Drawing.Font("Calibri", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnReset.Location = new System.Drawing.Point(111, 395);
+            this.btnReset.Name = "btnReset";
+            this.btnReset.Size = new System.Drawing.Size(184, 23);
+            this.btnReset.TabIndex = 8;
+            this.btnReset.Text = "FACTORY DATA RESET";
+            this.btnReset.UseVisualStyleBackColor = true;
+            this.btnReset.Click += new System.EventHandler(this.btnReset_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.DimGray;
+            this.ClientSize = new System.Drawing.Size(401, 452);
+            this.Controls.Add(this.btnReset);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Attendance Checker";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtID;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtCourse;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel1;
+        private System.Windows.Forms.ToolStripStatusLabel tsDT;
+        private System.Windows.Forms.Button btnReset;
+    }
+}
+
---
+++ AttendanceMySQL/Form1.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+
+namespace AttendanceMySQL
+{
+    public partial class Form1 : Form
+    {
+        private readonly string _connectionString = "server=localhost;username=root;database=db_attendance;password=tite";
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            tsDT.Text = DateTime.Now.ToString();
+            txtID.Focus();
+        }
+
+        private void IDEnter(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode != Keys.Enter)
+            {
+                return;
+            }
+            
+            var sql = "SELECT * FROM tbl_students WHERE id_number = @id";
+
+            var connection = new MySqlConnection(_connectionString);
+            var cmd = new MySqlCommand(sql, connection);
+            cmd.Parameters.AddWithValue("@id", txtID.Text);
+
+            try
+            {
+                connection.Open();
+                var reader = cmd.ExecuteReader();
+
+                if (reader.Read())
+                {
+                    MessageBox.Show($"The student {reader.GetString("Name")} from {reader.GetString("Course")} is Present!", "", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    txtID.Text = "";
+                    txtID.Focus();
+                }
+                else
+                {
+                    txtName.Enabled = true;
+                    txtName.Focus();
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Error: {ex.Message}", "", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void NameEnter(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                txtCourse.Enabled = true;
+                txtCourse.Focus();
+            }
+        }
+
+        private void CourseEnter(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode != Keys.Enter)
+            {
+                return;
+            }
+
+            var sql = "INSERT INTO tbl_students (id_number, Name, Course) VALUES (@id, @name, @course)";
+
+            var connection = new MySqlConnection(_connectionString);
+            var cmd = new MySqlCommand(sql, connection);
+            cmd.Parameters.AddWithValue("@id", txtID.Text);
+            cmd.Parameters.AddWithValue("@name", txtName.Text);
+            cmd.Parameters.AddWithValue("@course", txtCourse.Text);
+
+            try
+            {
+                connection.Open();
+                var i = cmd.ExecuteNonQuery();
+
+                if (i == 1)
+                {
+                    MessageBox.Show("The student has been registered!", "", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+                    txtID.Focus();
+                    txtName.Enabled = false;
+                    txtCourse.Enabled = false;
+                    txtID.Text = "";
+                    txtName.Text = "";
+                    txtCourse.Text = "";
+                }
+                else
+                {
+                    MessageBox.Show("Error Encountered!", "", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Error: {ex.Message}", "", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void btnReset_Click(object sender, EventArgs e)
+        {
+            var sql = "DELETE FROM tbl_students";
+
+            var connection = new MySqlConnection(_connectionString);
+            var cmd = new MySqlCommand(sql, connection);
+
+            try
+            {
+                connection.Open();
+                var i = cmd.ExecuteNonQuery();
+
+                MessageBox.Show($"{i} rows have been deleted from the table!", "", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Error: {ex.Message}", "", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+    }
+}
